aeb29fc0741f553650a54e5669c8c691419063c4
Grenish rai <107925840+Grenish@users.noreply.github.com>
2024-12-02 02:52:51 +0530

    Add Section component

---
+++ components/different-section.tsx
@@ -0,0 +1,22 @@
+export default function DifferentSection() {
+  return (
+    <div className="w-full h-screen background-1 relative">
+      <div className="p-20 pt-32">
+        <h1 className="text-7xl font-bold capitalize w-1/2 text-gray-300">
+          Designed to be different
+        </h1>
+        <p className="w-1/3 mt-2 text-gray-300/70 font-semibold">
+          Anarc's unique octagonal dial is crafted in stainless steel, paired
+          with a liquid silicone strap to bring together style and comfort like
+          never before.
+        </p>
+
+        <div className="absolute bottom-10 text-lg text-yellow-300/80">
+          <p className="">Designed in London</p>
+          <p>Functional Crown with haptics</p>
+          <p>Forged in Stainless Steel</p>
+        </div>
+      </div>
+    </div>
+  );
+}
