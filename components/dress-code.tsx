a99f17a160e4494b3f15cc6801a1dd96ccb3565e
Grenish rai <107925840+Grenish@users.noreply.github.com>
2024-12-02 02:53:00 +0530

    Add DressCode component

---
+++ components/dress-code.tsx
@@ -0,0 +1,24 @@
+export default function DressCode() {
+  return (
+    <div className="w-full p-20 flex flex-col h-screen justify-between background-2">
+      <div>
+        <h1 className="text-6xl font-bold capitalize w-1/3 text-gray-200">
+          Flaunt Your Style
+        </h1>
+        <p className="w-1/3 mt-2 text-gray-300/90 font-semibold text-balance">
+          Show off your personality with a sleek and stylish Anarc that
+          completes any look.
+        </p>
+      </div>
+      <div className="self-end text-right flex flex-col items-end">
+        <h1 className="text-6xl font-bold capitalize w-1/2 text-gray-200 ">
+          Own the Spotlight
+        </h1>
+        <p className="w-1/2 mt-2  text-gray-300/90 font-semibold">
+          Turn heads and shine bright with a bold design made to stand out
+          anywhere.
+        </p>
+      </div>
+    </div>
+  );
+}
