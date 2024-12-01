e393a11a8cf22f16d53be50c8eb18837a883cf0c
Grenish rai <107925840+Grenish@users.noreply.github.com>
2024-12-02 02:55:13 +0530

    Temporary refractor for testing components

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.aceternity.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'aceternity.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
