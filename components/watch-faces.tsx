b8cd9fd90ea67beb25b6a89dbf2cff5e5688d8a3
Grenish rai <107925840+Grenish@users.noreply.github.com>
2024-12-02 02:53:30 +0530

    Add WatchFaces component

---
+++ components/watch-faces.tsx
@@ -0,0 +1,80 @@
+import { HeroParallax } from "./ui/hero-parallax";
+
+export default function WatchFaces() {
+  return <HeroParallax products={products} />;
+}
+
+export const products = [
+  {
+    title: "Dynamic Dunes",
+    thumbnail: "/anarc16.png",
+  },
+  {
+    title: "Azure Blocks",
+    thumbnail: "/anarc17.png",
+  },
+  {
+    title: "Cosmic Swirl",
+    thumbnail: "/anarc18.png",
+  },
+  {
+    title: "Fragment Glow",
+    thumbnail: "/anarc19.png",
+  },
+  {
+    title: "Shadowed Dial",
+    thumbnail: "/anarc20.png",
+  },
+  {
+    title: "Ocean Ripples",
+    thumbnail: "/anarc21.png",
+  },
+  {
+    title: "Monochrome Morph",
+    thumbnail: "/anarc22.png",
+  },
+  {
+    title: "Chrono Sphere",
+    thumbnail: "/anarc23.png",
+  },
+  {
+    title: "Liquid Smoke",
+    thumbnail: "/anarc24.png",
+  },
+  {
+    title: "Gradient Orb",
+    thumbnail: "/anarc25.png",
+  },
+  {
+    title: "Zigzag Current",
+    thumbnail: "/anarc26.png",
+  },
+  {
+    title: "Spiral Vortex",
+    thumbnail: "/anarc27.png",
+  },
+  {
+    title: "Segmented Clock",
+    thumbnail: "/anarc28.png",
+  },
+  {
+    title: "Striped Horizon",
+    thumbnail: "/anarc29.png",
+  },
+  {
+    title: "Wavefronts",
+    thumbnail: "/anarc30.png",
+  },
+  {
+    title: "Crescent Layers",
+    thumbnail: "/anarc31.png",
+  },
+  {
+    title: "Curved Path",
+    thumbnail: "/anarc32.png",
+  },
+  {
+    title: "Linear Fade",
+    thumbnail: "/anarc33.png",
+  },
+];
