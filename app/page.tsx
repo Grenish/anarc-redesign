76c3caf2156470d2389523b22279d08b03ad57c5
Grenish rai <107925840+Grenish@users.noreply.github.com>
2024-12-02 02:52:32 +0530

    Refactor component imports in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import DifferentSection from "@/components/different-section";
+import DressCode from "@/components/dress-code";
+import GetToKnow from "@/components/get-to-know";
+import HeroSection from "@/components/hero-section";
 import Navbar from "@/components/navbar";
+import Specs from "@/components/specs";
+import WatchFaces from "@/components/watch-faces";
+import WatchSection from "@/components/watch-section";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <HeroSection />
+      <WatchSection />
+      <GetToKnow />
+      <DifferentSection />
+      <DressCode />
+      <WatchFaces />
+      <Specs />
     </main>
   );
 }
