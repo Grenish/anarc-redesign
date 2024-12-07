fc3651b3410255d4446d2d78f1e5d1457dd41cdf
Grenish rai <107925840+Grenish@users.noreply.github.com>
2024-12-07 16:29:56 +0530

    Refactor WatchSection component for improved layout and responsiveness

--- components/watch-section.tsx
+++ components/watch-section.tsx
@@ -2,26 +2,32 @@ import Image from "next/image";
 
 export default function WatchSection() {
   return (
-    <div className="bg-secondary flex items-center justify-between w-full">
-      <div className="w-1/2 flex flex-col items-center">
-        <div className="ml-32">
-          <h2 className="text-4xl capitalize font-black">Designed in london</h2>
-          <h2 className="text-4xl capitalize font-black">Made for india</h2>
-          <div className="w-full mt-2">
-            <p className="text-balance text-gray-600">
+    <div className="bg-secondary flex flex-col md:flex-row items-center justify-between w-full">
+      <div className="w-full md:w-1/2 flex flex-col items-center justify-center px-4 pt-3 sm:pt-2 md:px-0">
+        <div className="md:ml-16 lg:ml-32 text-center md:text-left max-w-xl">
+          <h2 className="text-2xl md:text-3xl lg:text-4xl capitalize font-black">
+            Designed in london
+          </h2>
+          <h2 className="text-2xl md:text-3xl lg:text-4xl capitalize font-black mt-2">
+            Made for india
+          </h2>
+          <div className="w-full mt-4">
+            <p className="text-balance text-gray-600 text-base md:text-lg">
               Beauty and geometry come together to create Anarc's unique
               octagonal dial.
             </p>
           </div>
         </div>
       </div>
-      <Image
-        src={"/anarc8.jpeg"}
-        alt="Anarc Watch"
-        width={1000}
-        height={1000}
-        className="w-1/2"
-      />
+      <div className="w-full md:w-1/2 mt-6 md:mt-0">
+        <Image
+          src={"/anarc8.jpeg"}
+          alt="Anarc Watch"
+          width={1000}
+          height={1000}
+          className="w-full h-auto object-cover"
+        />
+      </div>
     </div>
   );
 }
